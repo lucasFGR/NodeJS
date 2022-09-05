dd75983dfa0c462e2a6840599cbec81d116fd754
Zeer0e1 <lucasfreitasr@outlook.com>
2022-09-05 14:22:21 -0300

    Fundamenos e primeiro projeto

---
+++ finapi/src/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const app = express();
+app.use(express.json());
+const costumers = [];
+/**
+ * cpf = string
+ * name = string
+ * id = uuid
+ * statement = []
+ */
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const costomerAlreadyExits = costumers.some(
+    (costumer) => costumer.cpf === cpf
+  );
+
+  if (costomerAlreadyExits) {
+    return response.status(400).send({ error: "Costumer already exists" });
+  }
+
+  costumers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return response.status(201).send(costumers);
+});
+
+app.listen(1412);
