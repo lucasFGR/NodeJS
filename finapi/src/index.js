affd694ede34023e7e02f410516a9e5076efe72b
Zeer0e1 <lucasfreitasr@outlook.com>
2022-09-05 16:05:54 -0300

    Criando um middlware para validação se a conta existe ou não

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -2,17 +2,30 @@ const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 const app = express();
 app.use(express.json());
-const costumers = [];
+const customers = [];
+
+// Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).send({ error: "costumer not found" });
+  }
+  request.customer = customer;
+  return next();
+}
 /**
  * cpf = string
  * name = string
  * id = uuid
  * statement = []
  */
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
-  const costomerAlreadyExits = costumers.some(
+  const costomerAlreadyExits = customers.some(
     (costumer) => costumer.cpf === cpf
   );
 
@@ -20,20 +33,19 @@ app.post("/account", (request, response) => {
     return response.status(400).send({ error: "Costumer already exists" });
   }
 
-  costumers.push({
+  customers.push({
     cpf,
     name,
     id: uuidv4(),
     statement: [],
   });
 
-  return response.status(201).send(costumers);
+  return response.status(201).send(customers);
 });
 
-app.get("/statement/:cpf", (request, response) => {
-  const { cpf } = request.params;
-
-  const costumer = costumers.find(cost);
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+  return response.json(customer.statement);
 });
 
 app.listen(1412);
